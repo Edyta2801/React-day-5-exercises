f96d138a3dc7f5a5247f176a47afb899f7fed7f1
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-22 11:28:32 +0100

    add ForceUpdate ad modify

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from 'react'
+
 import Counter from './Counter'
+import ForceUpdate from './ForceUpdate'
+import Form from './Form'
 
 const App = (props) => (
   <div>
-    <h1>
-
-    </h1>
-    <Counter
+    <Counter 
       startValue={5}
       min={-10}
       max={10}
     />
-    <Counter/>
-    <ForUpdate>
-      
-    </ForUpdate>
+    <hr />
+    <Counter />
+    <hr />
+    <ForceUpdate />
+    <hr />
+    <Form />
   </div>
 )
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,59 +1,63 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 
 class Counter extends React.Component {
-    state = {
-        number: this.props.startValue
-    }
+  state = {
+    number: this.props.startValue
+  }
 
-    incHandler = () => {
-        if (this.state.number >= this.props.max) return
-        this.setState({ number: this.state.number + 1 })
-    }
-    decHandler = () => {
-        if (this.state.number <= this.props.max) return
-        this.setState({ number: this.state.number - 1 })
-    }
+  incHandler = () => {
+    if (this.state.number >= this.props.max) return
 
-    render() {
-        return (
-            <div>
-                <h1>
-                    {this.state.number}
-                </h1>
-                <button
-                    onClick={this.incHandler}
-                >
-                    +
-                </button>
-                <button
-                    onClick={this.decHandler}
-                >
-                    -
-                </button>
-                <div>
-                    {
-                        this.state.number === this.props.min?
-                    'Lower range exceeded!'
-                    :
-                    this.state.number===this.props.max?
-                    'Upper range exceeded!'
-                    :
-                    null
-                }
-                </div>
-            </div>      
-        )
-    }
+    this.setState({ number: this.state.number + 1 })
+  }
 
+  decHandler = () => {
+    if (this.state.number <= this.props.min) return
+
+    this.setState({ number: this.state.number - 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.number}</h1>
+        <button
+          onClick={this.incHandler}
+        >
+          +
+        </button>
+        <button
+          onClick={this.decHandler}
+        >
+          -
+        </button>
+        <div>
+          {
+            this.state.number === this.props.min ?
+              'Lower range exceeded!'
+              :
+              this.state.number === this.props.max ?
+                'Upper range exceeded!'
+                :
+                null
+          }
+        </div>
+      </div>
+    )
+  }
 }
+
 Counter.defaultProps = {
-    startValue: 0,
-    min: -5,
-    max: 5
+  startValue: 0,
+  min: -5,
+  max: 5
 }
-Counter.propTypes = {}
-startValue: propTypes.startValue
 
+Counter.propTypes = {
+  startValue: PropTypes.number.isRequired,
+  min: PropTypes.number,
+  max: PropTypes.number
 }
+
 export default Counter
