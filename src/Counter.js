bed11b30777737f8346aec0218da123a4c28c868
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-22 11:22:03 +0100

    modify files

---
+++ src/Counter.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+
+class Counter extends React.Component {
+    state = {
+        number: this.props.startValue
+    }
+
+    incHandler = () => {
+        if (this.state.number >= this.props.max) return
+        this.setState({ number: this.state.number + 1 })
+    }
+    decHandler = () => {
+        if (this.state.number <= this.props.max) return
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.number}
+                </h1>
+                <button
+                    onClick={this.incHandler}
+                >
+                    +
+                </button>
+                <button
+                    onClick={this.decHandler}
+                >
+                    -
+                </button>
+                <div>
+                    {
+                        this.state.number === this.props.min?
+                    'Lower range exceeded!'
+                    :
+                    this.state.number===this.props.max?
+                    'Upper range exceeded!'
+                    :
+                    null
+                }
+                </div>
+            </div>      
+        )
+    }
+
+}
+Counter.defaultProps = {
+    startValue: 0,
+    min: -5,
+    max: 5
+}
+Counter.propTypes = {}
+startValue: propTypes.startValue
+
+}
+export default Counter
